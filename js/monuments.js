4f89423bdd421573accb35319fc0e3197510ef1b
aleksanderstoyanov <blackeder3@gmail.com>
2024-02-24 18:42:21 +0200

    chore: add mobile rendering

--- js/monuments.js
+++ js/monuments.js
@@ -3,8 +3,6 @@ $(document).ready(function(){
         let target = $(e.currentTarget);
         let cardBody = $(target).nextAll(".card-body");
 
-        debugger;
-        console.log($.trim(target.text()));
         $.trim(target.text()) == "Виж още информация" ? $(cardBody).slideDown(700) : $(cardBody).slideUp(700);
         $.trim(target.text()) == "Виж още информация" ? target.text("Скрий информация") : target.text("Виж още информация");
     })
