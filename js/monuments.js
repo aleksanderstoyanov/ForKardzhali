f3423b04ae3cf06a779ca6f664a6b572eaed89d2
aleksanderstoyanov <blackeder3@gmail.com>
2024-02-25 09:41:35 +0200

    chore: add ui improvements

--- js/monuments.js
+++ js/monuments.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     $(".details-btn").on("click", function(e){
         let target = $(e.currentTarget);
-        let cardBody = $(target).nextAll(".card-body");
+        let cardBody = $(target).prevAll(".card-body");
 
         $.trim(target.text()) == "Виж още информация" ? $(cardBody).slideDown(700) : $(cardBody).slideUp(700);
         $.trim(target.text()) == "Виж още информация" ? target.text("Скрий информация") : target.text("Виж още информация");
